13393bdedce8a79cbf9ea729b44d0e72f2263662
Toomas Soome <tsoome@me.com>
2017-07-15 13:02:41 +0300

    ixgbe: this statement may fall through
    ../../common/io/ixgbe/core/ixgbe_common.c:250:6: error: this statement may fall through [-Werror=implicit-fallthrough=]
       if (ret_val != IXGBE_SUCCESS)
          ^
    ../../common/io/ixgbe/core/ixgbe_common.c:254:2: note: here
      case ixgbe_media_type_fiber_fixed:
      ^~~~
    ../../common/io/ixgbe/core/ixgbe_common.c: In function 'ixgbe_set_rxpba_generic':
    ../../common/io/ixgbe/core/ixgbe_common.c:4566:3: error: this statement may fall through [-Werror=implicit-fallthrough=]
       for (; i < (num_pb / 2); i++)
       ^~~
    ../../common/io/ixgbe/core/ixgbe_common.c:4569:2: note: here
      case PBA_STRATEGY_EQUAL:
      ^~~~
    
    ../../common/io/ixgbe/core/ixgbe_82599.c:1742:9: error: this statement may fall through [-Werror=implicit-fallthrough=]
       fdirm |= IXGBE_FDIRM_VLANID;
    ../../common/io/ixgbe/core/ixgbe_82599.c:1743:2: note: here
      case 0x0FFF:
      ^~~~
    ../../common/io/ixgbe/core/ixgbe_82599.c: In function 'ixgbe_fdir_add_perfect_filter_82599':
    ../../common/io/ixgbe/core/ixgbe_82599.c:2012:6: error: this statement may fall through [-Werror=implicit-fallthrough=]
       if (input->formatted.dst_port || input->formatted.src_port) {
          ^
    ../../common/io/ixgbe/core/ixgbe_82599.c:2016:2: note: here
      case IXGBE_ATR_FLOW_TYPE_TCPV4:
      ^~~~
    
    cc1: all warnings being treated as errors

--- usr/src/uts/common/io/ixgbe/core/ixgbe_common.c
+++ usr/src/uts/common/io/ixgbe/core/ixgbe_common.c
@@ -250,7 +250,7 @@ s32 ixgbe_setup_fc_generic(struct ixgbe_hw *hw)
 		if (ret_val != IXGBE_SUCCESS)
 			goto out;
 
-		/* only backplane uses autoc so fall though */
+		/* FALLTHROUGH */
 	case ixgbe_media_type_fiber_fixed:
 	case ixgbe_media_type_fiber_qsfp:
 	case ixgbe_media_type_fiber:
@@ -4565,7 +4565,7 @@ void ixgbe_set_rxpba_generic(struct ixgbe_hw *hw, int num_pb, u32 headroom,
 		rxpktsize <<= IXGBE_RXPBSIZE_SHIFT;
 		for (; i < (num_pb / 2); i++)
 			IXGBE_WRITE_REG(hw, IXGBE_RXPBSIZE(i), rxpktsize);
-		/* Fall through to configure remaining packet buffers */
+		/* FALLTHROUGH */
 	case PBA_STRATEGY_EQUAL:
 		rxpktsize = (pbsize / (num_pb - i)) << IXGBE_RXPBSIZE_SHIFT;
 		for (; i < num_pb; i++)
