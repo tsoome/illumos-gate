8fc8a13f7464244f8a6550c34caf6b546b786630
Toomas Soome <tsoome@me.com>
2020-02-01 11:27:27 +0200

    ontario: smatch errors
    ../../../sun4v/ontario/io/tsalarm.c: In function 'tsalarm_attach':
    ../../../sun4v/ontario/io/tsalarm.c:413: error: passing argument 6 of 'ddi_create_minor_node' makes integer from pointer without a cast
    ../../../common/sys/sunddi.h:1571: note: expected 'int' but argument is of type 'void *'
    ../../../sun4v/ontario/io/tsalarm.c: In function 'glvc_alarm_get':
    ../../../sun4v/ontario/io/tsalarm.c:752: error: assignment makes integer from pointer without a cast
    ../../../sun4v/ontario/io/tsalarm.c: In function 'glvc_alarm_set':
    ../../../sun4v/ontario/io/tsalarm.c:827: error: assignment makes integer from pointer without a cast
    
    /code/illumos-gate/usr/src/tools/proto/root_sparc-nd/opt/onbld/bin/sparc/smatch: ../../../sun4v/ontario/os/ontario.c:92 load_platform_drivers() warn: ignoring unreachable code.
    /code/illumos-gate/usr/src/tools/proto/root_sparc-nd/opt/onbld/bin/sparc/smatch: ../../../sun4v/ontario/os/ontario.c:97 load_platform_drivers() warn: ignoring unreachable code.

--- usr/src/uts/sun4v/ontario/os/ontario.c
+++ usr/src/uts/sun4v/ontario/os/ontario.c
@@ -25,8 +25,6 @@
  * Use is subject to license terms.
  */
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/sysmacros.h>
@@ -89,12 +87,10 @@ load_platform_drivers(void)
 	dip = e_ddi_hold_devi_by_path(ONTARIO_ISA_PATHNAME, 0);
 	if (dip == NULL) {
 		cmn_err(CE_PANIC, "Could not install the isa driver\n");
-		return;
 	}
 
 	if (pci_config_setup(dip, &isa_handle) != DDI_SUCCESS) {
 		cmn_err(CE_PANIC, "Could not get the config space of isa\n");
-		return;
 	}
 }
 
