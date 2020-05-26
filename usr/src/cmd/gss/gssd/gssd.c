54b3163adb4c181b8a09b3943d2f6440a34f8d21
Toomas Soome <tsoome@me.com>
2020-05-26 12:06:20 +0300

    12791 rpcgen: if main is not provided, _svcstate_lock should be extern
    When we do generate svc stubs without main, we should declare
    mutex_t _svcstate_lock as extern, so we can decide where we are going to
    instatiate it.
    
    This change does also reveal issues with some of the applications:
    
    fmd: missing mutex_init()
    gssd: missing mutex_init()
    kwarn: missing mutex_init()
    idmapd: needs _svcstate_lock to be added
    
    Change-Id: Id1b203f8254b82209751c60358ac14b40f9a48eb

--- usr/src/cmd/gss/gssd/gssd.c
+++ usr/src/cmd/gss/gssd/gssd.c
@@ -23,8 +23,6 @@
  * Use is subject to license terms.
  */
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
-
 /*
  * Usermode daemon which assists the kernel when handling gssapi calls.
  * It is gssd that actually implements all gssapi calls.
@@ -62,7 +60,7 @@ extern int  svc_create_local_service();
 int _rpcpmstart = 0;		/* Started by a port monitor ? */
 int _rpcfdtype;			/* Whether Stream or Datagram ? */
 int _rpcsvcdirty;		/* Still serving ? */
-
+mutex_t _svcstate_lock;
 
 static void
 /* LINTED */
@@ -105,7 +103,7 @@ char **argv;
 	setlocale(LC_ALL, "");
 	textdomain(TEXT_DOMAIN);
 
-
+	mutex_init(&_svcstate_lock, USYNC_THREAD, NULL);
 	/*
 	 * Take special note that "getuid()" is called here.  This call is used
 	 * rather than app_krb5_user_uid(), to ensure gssd(1M) is running as
