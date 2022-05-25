52c7990b3034ff3875d5ea57062c4938c1910567
Alexander Motin <mav@FreeBSD.org>
2022-05-25 13:12:52 -0400

    More speculative prefetcher improvements
     - Make prefetch distance adaptive: up to 4MB prefetch doubles for
    every, hit same as before, but after that it grows by 1/8 every time
    the prefetch read does not complete in time to satisfy the demand.
    My tests show that 4MB is sufficient for wide NVMe pool to saturate
    single reader thread at 2.5GB/s, while new 64MB maximum allows the
    same thread to reach 1.5GB/s on wide HDD pool.  Further distance
    increase may increase speed even more, but less dramatic and with
    higher latency.
    
     - Allow early reuse of inactive prefetch streams: streams that never
    saw hits can be reused immediately if there is a demand, while others
    can be reused after 1s of inactivity, starting with the oldest.  After
    2s of inactivity streams are deleted to free resources same as before.
    This allows by several times increase strided read performance on HDD
    pool in presence of simultaneous random reads, previously filling the
    zfetch_max_streams limit for seconds and so blocking most of prefetch.
    
     - Always issue intermediate indirect block reads with SYNC priority.
    Each of those reads if delayed for longer may delay up to 1024 other
    block prefetches, that may be not good for wide pools.
    
    Reviewed-by: Allan Jude <allan@klarasystems.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Alexander Motin <mav@FreeBSD.org>
    Sponsored-By: iXsystems, Inc.
    Closes #13452
    Change-Id: I9ffc7124f4162a6cf687c1bb919de5bd0939358b

--- usr/src/uts/common/fs/zfs/sys/dbuf.h
+++ usr/src/uts/common/fs/zfs/sys/dbuf.h
@@ -308,7 +308,7 @@ typedef struct dbuf_hash_table {
 	kmutex_t hash_mutexes[DBUF_MUTEXES];
 } dbuf_hash_table_t;
 
-typedef void (*dbuf_prefetch_fn)(void *, boolean_t);
+typedef void (*dbuf_prefetch_fn)(void *, uint64_t, uint64_t, boolean_t);
 
 uint64_t dbuf_whichblock(struct dnode *di, int64_t level, uint64_t offset);
 
