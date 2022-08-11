a7dcd153dd3e6806d2226746510bfb1f9cdaaa88
Warner Losh <imp@FreeBSD.org>
2022-08-11 09:07:44 -0600

    stand: Replace zfs_fmtdev with generic devformat()
    Sponsored by:           Netflix
    Differential Revision:  https://reviews.freebsd.org/D35973
    
    Change-Id: I8d82326a956a5728445ac2f104030285fbe6328c

--- usr/src/boot/libsa/zfs/zfs.c
+++ usr/src/boot/libsa/zfs/zfs.c
@@ -1591,7 +1591,7 @@ zfs_dev_open(struct open_file *f, ...)
 	rv = 0;
 	/* This device is not set as currdev, mount us private copy. */
 	if (mount == NULL)
-		rv = zfs_mount(zfs_fmtdev(&dev->dd), NULL, (void **)&mount);
+		rv = zfs_mount(devformat(&dev->dd), NULL, (void **)&mount);
 
 	f->f_mount = mount;
 
