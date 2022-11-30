2bb736a222e80280cbc0b2de31b5d3ad4dc4f393
Warner Losh <imp@FreeBSD.org>
2022-11-30 15:09:36 -0700

    stand: parsedev API change: devspec now points to start of full device name
    To support more flexible device matching, we now pass in the full
    devspec to the parsedev routines. For everything execpt uboot, this is
    just a drop in (since everything except uboot and openfirmware always
    uses disk...: and/or zfs:, but openfirmware isn't really affected).
    
    uboot we kludge around it by subtracting 4 from where the rest of the
    device name starts. This is unforunate, and can compute the address one
    before the string. But we never dereference that address. uboot needs
    more work, and this is an acceptable UB until that other work happens.
    
    OFW doesn't really use the parsedev routines these days (since none of
    the supported device uses this... yet). It too needs more work, but it
    needs device matching support first.
    
    Sponsored by:           Netflix
    Reviewed by:            delphij
    Differential Revision:  https://reviews.freebsd.org/D37553
    
    Change-Id: If27056172b2091cf5a8c5bf3e32a1acc562a1921

--- usr/src/boot/libsa/zfs/zfs.c
+++ usr/src/boot/libsa/zfs/zfs.c
@@ -357,7 +357,7 @@ zfs_mount(const char *dev, const char *path, void **data)
 	int rv;
 
 	errno = 0;
-	rv = zfs_parsedev((struct devdesc **)&zfsdev, dev + 3, NULL);
+	rv = zfs_parsedev((struct devdesc **)&zfsdev, dev, NULL);
 	if (rv != 0) {
 		return (rv);
 	}
@@ -1652,7 +1652,7 @@ zfs_parsedev(struct devdesc **idev, const char *devspec, const char **path)
 	int		rv;
 	struct zfs_devdesc *dev;
 
-	np = devspec;
+	np = devspec + 3;
 	if (*np != ':')
 		return (EINVAL);
 	np++;
