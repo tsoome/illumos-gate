87a6b1098cd3ce5b60a6667891af2ba5ad6b5de9
Richard Lowe <richlowe@richlowe.net>
2023-01-07 13:58:15 -0600

    correctly pick apart PLT entries in `librtld_db`
    This is the code which underpins annotating disassembly in mdb to indicate
    that a branch is to a PLT entry, and whether that PLT entry is already bound.
    
    We have two options for implementing this, one is to -- like on ia32 --
    iterate every loaded object to find the right one and then to match our index
    in .plt (which we know) to our index in .got.plt (which we don't).
    
    The other is to disassemble the PLT entry to pull out the target address, and
    check the target address.  This is the option we have taken right now, even
    though it's not entirely pleasing.
    
    At present, this won't handle BTI, PAC, PACBTI, or anything else that creates
    an alternate PLT sequence.  Luckily, we also don't support anything that does
    that yet...
    
    This might change in future depending on the easiest way to handle PAC and
    BTI.

--- usr/src/cmd/sgs/librtld_db/aarch64/plt64_resolution.c
+++ usr/src/cmd/sgs/librtld_db/aarch64/plt64_resolution.c
@@ -24,31 +24,74 @@
  * Use is subject to license terms.
  */
 
+#include <sys/debug.h>
+#include <sys/reg.h>
 
-#include	<libelf.h>
-#include	<sys/reg.h>
-#include	<rtld_db.h>
-#include	"_rtld_db.h"
-#include	"msg.h"
+#include <libelf.h>
 
-static int next_offset(uint32_t insn)
+#include <rtld_db.h>
+#include "_rtld_db.h"
+#include "msg.h"
+
+#define	ADRP_IMMHI_SHIFT	5
+#define	ADRP_IMMHI_MASK		(0x7ffff << ADRP_IMMHI_SHIFT)
+#define	ADRP_IMMHI(insn)	((insn & ADRP_IMMHI_MASK) >> ADRP_IMMHI_SHIFT)
+
+#define	ADRP_IMMLO_SHIFT	29
+#define	ADRP_IMMLO_MASK		(0x3 << ADRP_IMMLO_SHIFT)
+#define	ADRP_IMMLO(insn)	((insn & ADRP_IMMLO_MASK) >> ADRP_IMMLO_SHIFT)
+
+#define	ADD_IMM_SHIFT	10
+#define	ADD_IMM_MASK	(0xfff << ADD_IMM_SHIFT)
+#define	ADD_IMM(insn)	((insn & ADD_IMM_MASK) >> ADD_IMM_SHIFT)
+
+/*
+ * Given a pointer to the start of a PLT entry, return the address of the
+ * entry in .got.plt.
+ *
+ * We could do this by the equivalence of their index by looking up the right
+ * object's .got.plt in a manner similar how we do this on ia32, but we
+ * instead disassemble to get the pointer directly.
+ *
+ * XXXARM: This, and many other things, will need work for BTI and PAC.
+ */
+static uintptr_t
+got_plt_addr(psaddr_t pc, uint32_t *insn)
 {
-	int offset;
-	offset = (insn & 0x001fffff);
-	if (offset & 0x00100000)
-		offset  |= 0xffe00000;
-	offset *= 4;
-	return offset;
+	uintptr_t addr, immhi, immlo;
+
+	VERIFY((*insn & 0x9f000000) == 0x90000000); /* an adrp */
+
+	/* Extract the immediate from the adrp  */
+	immhi = ADRP_IMMHI(*insn);
+	immlo = ADRP_IMMLO(*insn);
+	addr = (pc + ((immhi << 14) | (immlo << 12))) & ~0xfff;
+
+	insn += 2;		/* Skip to the add */
+	VERIFY((*insn & 0x7f800000) == 0x11000000); /* an immediate add */
+
+	return (addr + ADD_IMM(*insn));
 }
 
+/*
+ * On AArch64 a PLT entry looks like this:
+ *
+ *   .plt+0x20:                 d0 00 00 90  adrp x16, 0x41a000
+ *   .plt+0x24:                 11 36 40 f9  ldr x17, [x16, #104] ; GOT.PLT[N]
+ *   .plt+0x28:                 10 a2 01 91  add x16, x16, #0x68  ; GOT.PLT[N]
+ *   .plt+0x2c:                 20 02 1f d6  br x17
+ *
+ *  The first time around GOT.PLT[N] contains address of PLT[0]; this forces
+ *	the first call to go thru elf_rtbndr.
+ *  Other times around the GOT.PLT[N] actually contains the resolved
+ *	address of the symbol(name), so the branch is direct
+ */
 rd_err_e
 plt64_resolution(rd_agent_t *rap, psaddr_t pc, lwpid_t lwpid,
-	psaddr_t pltbase, rd_plt_info_t *rpi)
+    psaddr_t pltbase, rd_plt_info_t *rpi)
 {
-	instr_t		instr[3];
-	psaddr_t	addr;
-	psaddr_t	pltoff, pltaddr;
-
+	instr_t		instr[3]; /* XXXARM: This is too small for BTI/PAC */
+	psaddr_t	pltoff, pltaddr, gotaddr, targetaddr;
 
 	if (rtld_db_version >= RD_VERSION3) {
 		rpi->pi_flags = 0;
@@ -59,20 +102,28 @@ plt64_resolution(rd_agent_t *rap, psaddr_t pc, lwpid_t lwpid,
 	pltaddr = pltbase + ((pltoff / M_PLT_ENTSIZE) * M_PLT_ENTSIZE);
 
 	/*
-	 * This is the target of the jmp instruction
+	 * This is the target of the branch instruction
 	 */
-	if (ps_pread(rap->rd_psp, pltaddr, (char *)instr, sizeof (instr)) != PS_OK) {
+	if (ps_pread(rap->rd_psp, pltaddr, (char *)instr,
+	    sizeof (instr)) != PS_OK) {
 		LOG(ps_plog(MSG_ORIG(MSG_DB_READFAIL_2), EC_ADDR(pltaddr)));
 		return (RD_ERR);
 	}
 
-	addr = pltaddr + 4 + next_offset(instr[0]);
+	gotaddr = got_plt_addr(pltaddr, instr);
 
-	if (addr == (pltbase - M_PLT_RESERVSZ)) {
+	if (ps_pread(rap->rd_psp, gotaddr, &targetaddr,
+	    sizeof (targetaddr)) != PS_OK) {
+		LOG(ps_plog(MSG_ORIG(MSG_DB_READFAIL_2), EC_ADDR(gotaddr)));
+		return (RD_ERR);
+	}
+
+	if (targetaddr == pltbase) {
 		rd_err_e	rerr;
+
 		/*
-		 * If GOT[ind] points to PLT+6 then this is the first
-		 * time through this PLT.
+		 * If GOT[ind] points to PLT[0] then this is the first
+		 * time through this PLT entry.
 		 */
 		if ((rerr = rd_binder_exit_addr(rap, MSG_ORIG(MSG_SYM_RTBIND),
 		    &(rpi->pi_target))) != RD_OK) {
@@ -90,7 +141,7 @@ plt64_resolution(rd_agent_t *rap, psaddr_t pc, lwpid_t lwpid,
 		rpi->pi_target = 0;
 		if (rtld_db_version >= RD_VERSION3) {
 			rpi->pi_flags |= RD_FLG_PI_PLTBOUND;
-			rpi->pi_baddr = addr;
+			rpi->pi_baddr = targetaddr;
 		}
 	}
 
