f63dd6af9d38961215ce8b007213e1235c7199b1
Toomas Soome <tsoome@me.com>
2023-09-25 23:29:50 +0300

    tmpfs: impossible condition
    /code/illumos-gate/usr/src/tools/proto/root_i386-nd/opt/onbld/bin/i386/smatch: .
    ./../common/fs/tmpfs/tmp_vnops.c:2193 tmp_map() warn: impossible condition '(off
     > 9223372036854775807) => (s64min-s64max > s64max)'
    /code/illumos-gate/usr/src/tools/proto/root_i386-nd/opt/onbld/bin/i386/smatch: .
    ./../common/fs/tmpfs/tmp_vnops.c:2341 tmp_space() warn: impossible condition '(b
    fp->l_start > 9223372036854775807) => (s64min-s64max > s64max)'
    /code/illumos-gate/usr/src/tools/proto/root_i386-nd/opt/onbld/bin/i386/smatch: .
    ./../common/fs/tmpfs/tmp_vnops.c:2341 tmp_space() warn: impossible condition '(b
    fp->l_len > 9223372036854775807) => (s64min-s64max > s64max)'
    /code/illumos-gate/usr/src/tools/proto/root_i386-nd/opt/onbld/bin/i386/smatch: .
    ./../common/fs/tmpfs/tmp_vnops.c:2359 tmp_seek() warn: impossible condition '(*n
    offp > 9223372036854775807) => (s64min-s64max > s64max)'
    
    Change-Id: Ib132f6b42bd7489668bb2a9adc87f7f80ba50df7

--- usr/src/uts/common/fs/tmpfs/tmp_vnops.c
+++ usr/src/uts/common/fs/tmpfs/tmp_vnops.c
@@ -2190,7 +2190,7 @@ tmp_map(
 		return (ENOSYS);
 
 	if (off < 0 || (offset_t)(off + len) < 0 ||
-	    off > MAXOFF_T || (off + len) > MAXOFF_T)
+	    (off + len) > MAXOFF_T)
 		return (ENXIO);
 
 	if (vp->v_type != VREG)
@@ -2338,8 +2338,6 @@ tmp_space(
 	if (cmd != F_FREESP)
 		return (EINVAL);
 	if ((error = convoff(vp, bfp, 0, (offset_t)offset)) == 0) {
-		if ((bfp->l_start > MAXOFF_T) || (bfp->l_len > MAXOFF_T))
-			return (EFBIG);
 		error = tmp_freesp(vp, bfp, flag);
 
 		if (error == 0 && bfp->l_start == 0)
@@ -2356,7 +2354,7 @@ tmp_seek(
 	offset_t *noffp,
 	caller_context_t *ct)
 {
-	return ((*noffp < 0 || *noffp > MAXOFFSET_T) ? EINVAL : 0);
+	return (*noffp < 0 ? EINVAL : 0);
 }
 
 /* ARGSUSED2 */
