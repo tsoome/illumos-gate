9af51520e64083a38ff03b8fef0a5b48d9261352
Richard Lowe <richlowe@richlowe.net>
2026-09-11 21:37:51 +0000

    Backout: 18149 ld(1) should support -zsysroot Reviewed by: Andy Fiddaman <illumos@fiddaman.net> Approved by: Robert Mustacchi <rm+illumos@fingolfin.org>

--- usr/src/cmd/sgs/include/libld.h
+++ usr/src/cmd/sgs/include/libld.h
@@ -411,7 +411,6 @@ struct ofl_desc {
 	int		ofl_aslr;	/* -z aslr, -1 disable, 1 enable */
 	APlist		*ofl_symasserts; /* assertions about symbols */
 					/* from mapfiles */
-	const char	*ofl_sysroot;	/* sysroot to use, if not the default */
 };
 
 #define	FLG_OF_DYNAMIC	0x00000001	/* generate dynamic output module */
--- usr/src/cmd/sgs/libld/common/ldmain.c
+++ usr/src/cmd/sgs/libld/common/ldmain.c
@@ -60,10 +60,9 @@ Target		ld_targ;
  * part of the path processing.
  */
 #if	defined(_ELF64)
-static char	def_Plibpath[] = "$SYSROOT/lib/64:$SYSROOT/usr/lib/64";
+static char	def_Plibpath[] = "/lib/64:/usr/lib/64";
 #else
-static char	def_Plibpath[] = "$SYSROOT/usr/ccs/lib:$SYSROOT/lib:"
-	"$SYSROOT/usr/lib";
+static char	def_Plibpath[] = "/usr/ccs/lib:/lib:/usr/lib";
 #endif
 
 /*
